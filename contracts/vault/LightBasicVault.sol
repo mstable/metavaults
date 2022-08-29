c0a19f3e09a06f38aabba88a9f763af30be93817
tushargargght <tushargargght@gmail.com>
2022-08-29 16:27:09 +0530

    feat: LightBasicVault for testing LightAbstractVault

---
+++ contracts/vault/LightBasicVault.sol
@@ -0,0 +1,249 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.16;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+import { InitializableToken } from "../tokens/InitializableToken.sol";
+import { VaultManagerRole } from "../shared/VaultManagerRole.sol";
+import { LightAbstractVault } from "./LightAbstractVault.sol";
+
+/**
+ * @title   A minimal implementation of a ERC-4626 vault.
+ * @author  mStable
+ * @notice  Basic Implementation predominantly for testing purpose
+ *
+ * @dev     VERSION: 1.0
+ *          DATE:    2022-08-29
+ */
+contract BasicVault is LightAbstractVault, Initializable {
+    using SafeERC20 for IERC20;
+
+    /**
+     * @param _nexus    Address of the Nexus contract that resolves protocol modules and roles.
+     * @param _asset    Address of the vault's asset.
+     */
+    constructor(address _nexus, address _asset)
+        LightAbstractVault(_asset)
+        VaultManagerRole(_nexus)
+    {}
+
+    function initialize(
+        string calldata _nameArg,
+        string calldata _symbolArg,
+        address _vaultManager
+    ) external initializer {
+        // Set the vault's decimals to the same as the reference asset.
+        uint8 _decimals = InitializableToken(address(_asset)).decimals();
+        InitializableToken._initialize(_nameArg, _symbolArg, _decimals);
+        VaultManagerRole._initialize(_vaultManager);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        DEPOSIT/MINT
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(uint256 assets, address receiver)
+        external
+        virtual
+        override
+        returns (uint256 shares)
+    {
+        shares = _deposit(assets, receiver);
+    }
+
+    function _deposit(uint256 assets, address receiver) internal virtual returns (uint256 shares) {
+        require((shares = _previewDeposit(assets)) != 0, "Shares are zero");
+
+        _transferAndMint(assets, shares, receiver);
+    }
+
+    function previewDeposit(uint256 assets) external view override returns (uint256 shares) {
+        shares = _previewDeposit(assets);
+    }
+
+    function _previewDeposit(uint256 assets) internal view virtual returns (uint256 shares) {
+        shares = _convertToShares(assets);
+    }
+
+    function mint(uint256 shares, address receiver)
+        external
+        virtual
+        override
+        returns (uint256 assets)
+    {
+        assets = _mint(shares, receiver);
+    }
+
+    function _mint(uint256 shares, address receiver) internal virtual returns (uint256 assets) {
+        require((assets = _previewMint(shares)) != 0, "Assets are zero");
+
+        _transferAndMint(assets, shares, receiver);
+    }
+
+    function previewMint(uint256 shares) external view override returns (uint256 assets) {
+        assets = _previewMint(shares);
+    }
+
+    function _previewMint(uint256 shares) internal view virtual returns (uint256 assets) {
+        assets = _convertToAssets(shares);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        INTERNAL DEPSOIT/MINT
+    //////////////////////////////////////////////////////////////*/
+
+    function _transferAndMint(
+        uint256 assets,
+        uint256 shares,
+        address receiver
+    ) internal virtual {
+        _asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        WITHDRAW/REDEEM
+    //////////////////////////////////////////////////////////////*/
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) external virtual override returns (uint256 shares) {
+        shares = _withdraw(assets, receiver, owner);
+    }
+
+    function _withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) internal virtual returns (uint256 shares) {
+        require((shares = _previewWithdraw(assets)) != 0, "Shares are zero");
+
+        _burnTransfer(assets, shares, receiver, owner);
+    }
+
+    function previewWithdraw(uint256 assets) external view override returns (uint256 shares) {
+        shares = _previewWithdraw(assets);
+    }
+
+    function _previewWithdraw(uint256 assets) internal view virtual returns (uint256 shares) {
+        shares = _convertToShares(assets);
+    }
+
+    function maxWithdraw(address owner) external view override returns (uint256 maxAssets) {
+        maxAssets = _maxWithdraw(owner);
+    }
+
+    function _maxWithdraw(address owner) internal view virtual returns (uint256 maxAssets) {
+        maxAssets = _previewRedeem(balanceOf(owner));
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) external virtual override returns (uint256 assets) {
+        assets = _redeem(shares, receiver, owner);
+    }
+
+    function _redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) internal virtual returns (uint256 assets) {
+        require((assets = _previewRedeem(shares)) != 0, "Assets are zero");
+
+        _burnTransfer(assets, shares, receiver, owner);
+    }
+
+    function previewRedeem(uint256 shares) external view override returns (uint256 assets) {
+        assets = _previewRedeem(shares);
+    }
+
+    function _previewRedeem(uint256 shares) internal view virtual returns (uint256 assets) {
+        assets = _convertToAssets(shares);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        INTERNAL WITHDRAW/REDEEM
+    //////////////////////////////////////////////////////////////*/
+
+    function _burnTransfer(
+        uint256 assets,
+        uint256 shares,
+        address receiver,
+        address owner
+    ) internal virtual {
+        // If caller is not the owner of the shares
+        uint256 allowed = allowance(owner, msg.sender);
+        if (msg.sender != owner && allowed != type(uint256).max) {
+            require(shares <= allowed, "Amount exceeds allowance");
+            _approve(owner, msg.sender, allowed - shares);
+        }
+
+        _burn(owner, shares);
+
+        _asset.safeTransfer(receiver, assets);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            EXTENRAL ASSETS
+    //////////////////////////////////////////////////////////////*/
+
+    function totalAssets() public view override returns (uint256 totalManagedAssets) {
+        totalManagedAssets = _asset.balanceOf(address(this));
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            CONVERTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function convertToAssets(uint256 shares)
+        external
+        view
+        virtual
+        override
+        returns (uint256 assets)
+    {
+        assets = _convertToAssets(shares);
+    }
+
+    function _convertToAssets(uint256 shares) internal view virtual returns (uint256 assets) {
+        uint256 totalShares = totalSupply();
+
+        if (totalShares == 0) {
+            assets = shares; // 1:1 value of shares and assets
+        } else {
+            assets = (shares * totalAssets()) / totalShares;
+        }
+    }
+
+    function convertToShares(uint256 assets)
+        external
+        view
+        virtual
+        override
+        returns (uint256 shares)
+    {
+        shares = _convertToShares(assets);
+    }
+
+    function _convertToShares(uint256 assets) internal view virtual returns (uint256 shares) {
+        uint256 totalShares = totalSupply();
+
+        if (totalShares == 0 || _asset.balanceOf(address(this)) == 0) {
+            shares = assets; // 1:1 value of shares and assets
+        } else {
+            shares = (assets * totalShares) / totalAssets();
+        }
+    }
+}
