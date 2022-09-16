567f12916fd2d4b545cff69e5cdbf33d23d24c10
Nicholas Addison <nick@addisonbrown.com.au>
2022-09-16 23:10:37 +1000

    chore: unit tests for SingleSlotMapper

---
+++ test/shared/SingleSlotMapper.spec.ts
@@ -0,0 +1,125 @@
+import { logger } from "@tasks/utils/logger"
+import { expect } from "chai"
+import { ethers } from "hardhat"
+import { TestSingleSlotMapper__factory } from "types/generated"
+
+import type { BN } from "@utils/math"
+import type { TestSingleSlotMapper } from "types/generated"
+
+const log = logger("test:SingleSlotMapper")
+
+describe("SingleSlotMapper", () => {
+    let mapper: TestSingleSlotMapper
+
+    before(async () => {
+        const accounts = await ethers.getSigners()
+        mapper = await new TestSingleSlotMapper__factory(accounts[0]).deploy()
+    })
+    it("initialize", async () => {
+        const mapData = await mapper.init()
+        expect(await mapper.indexes(mapData)).to.eq(0)
+        expect(await mapper.map(mapData, 0)).to.eq(0xf)
+        expect(await mapper.map(mapData, 1)).to.eq(0xf)
+        expect(await mapper.map(mapData, 61)).to.eq(0xf)
+        await expect(mapper.map(mapData, 62)).to.rejectedWith("Index out of bounds")
+    })
+    describe("should add first value", async () => {
+        let mapData: BN
+        let index
+        beforeEach(async () => {
+            mapData = await mapper.init()
+        })
+        const testValues = [0, 1, 2, 14, 0xe]
+        testValues.forEach((value) => {
+            it(`${value}`, async () => {
+                ;({ index, mapData_: mapData } = await mapper.addValue(mapData, value))
+                expect(index).to.eq(0)
+                expect(await mapper.map(mapData, 0)).to.eq(value)
+                expect(await mapper.map(mapData, 1)).to.eq(0xf)
+                expect(await mapper.map(mapData, 61)).to.eq(0xf)
+                expect(await mapper.indexes(mapData)).to.eq(1)
+            })
+        })
+    })
+    it("fail to add invalid first value", async () => {
+        const mapData = await mapper.init()
+        await expect(mapper.addValue(mapData, 0xf)).to.rejectedWith("value out of bounds")
+        await expect(mapper.addValue(mapData, 0x10)).to.rejectedWith("value out of bounds")
+        await expect(mapper.addValue(mapData, 0xf0)).to.rejectedWith("value out of bounds")
+        await expect(mapper.addValue(mapData, 0x100)).to.rejectedWith("value out of bounds")
+        await expect(mapper.addValue(mapData, 0xf00)).to.rejectedWith("value out of bounds")
+    })
+    describe("should add three values", async () => {
+        let mapData: BN
+        let index
+        beforeEach(async () => {
+            mapData = await mapper.init()
+        })
+        const testValues = [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 2],
+            [14, 13, 12],
+            [14, 14, 14],
+        ]
+        testValues.forEach((values) => {
+            it(`${values}`, async () => {
+                // Add first value
+                ;({ index, mapData_: mapData } = await mapper.addValue(mapData, values[0]))
+                expect(index).to.eq(0)
+                expect(await mapper.map(mapData, 0)).to.eq(values[0])
+                expect(await mapper.map(mapData, 1)).to.eq(0xf)
+                expect(await mapper.map(mapData, 2)).to.eq(0xf)
+                expect(await mapper.map(mapData, 3)).to.eq(0xf)
+                expect(await mapper.map(mapData, 4)).to.eq(0xf)
+                expect(await mapper.map(mapData, 61)).to.eq(0xf)
+                expect(await mapper.indexes(mapData)).to.eq(1)
+                // Add second value
+                ;({ index, mapData_: mapData } = await mapper.addValue(mapData, values[1]))
+                expect(index).to.eq(1)
+                expect(await mapper.map(mapData, 0)).to.eq(values[0])
+                expect(await mapper.map(mapData, 1)).to.eq(values[1])
+                expect(await mapper.map(mapData, 2)).to.eq(0xf)
+                expect(await mapper.map(mapData, 3)).to.eq(0xf)
+                expect(await mapper.map(mapData, 4)).to.eq(0xf)
+                expect(await mapper.map(mapData, 61)).to.eq(0xf)
+                expect(await mapper.indexes(mapData)).to.eq(2)
+                // Add third value
+                ;({ index, mapData_: mapData } = await mapper.addValue(mapData, values[2]))
+                expect(index).to.eq(2)
+                expect(await mapper.map(mapData, 0)).to.eq(values[0])
+                expect(await mapper.map(mapData, 1)).to.eq(values[1])
+                expect(await mapper.map(mapData, 2)).to.eq(values[2])
+                expect(await mapper.map(mapData, 3)).to.eq(0xf)
+                expect(await mapper.map(mapData, 4)).to.eq(0xf)
+                expect(await mapper.map(mapData, 61)).to.eq(0xf)
+                expect(await mapper.indexes(mapData)).to.eq(3)
+            })
+        })
+    })
+    it("should add 62 values", async () => {
+        let mapData = await mapper.init()
+        const indexes = [...Array(62).keys()]
+        for (const index of indexes) {
+            const value = index % 15
+            log(`index: ${index}, value: ${value}`)
+
+            const result = await mapper.addValue(mapData, value)
+
+            mapData = result.mapData_
+            expect(result.index).to.eq(index)
+            expect(await mapper.map(mapData, index)).to.eq(value)
+            expect(await mapper.indexes(mapData)).to.eq(index + 1)
+        }
+    })
+    it("fail to add 63 values", async () => {
+        let mapData = await mapper.init()
+        const indexes = [...Array(62).keys()]
+        for (const index of indexes) {
+            const value = (index + 1) % 15
+            const result = await mapper.addValue(mapData, value)
+            mapData = result.mapData_
+        }
+        await expect(mapper.addValue(mapData, 1)).to.rejectedWith("map full")
+    })
+})
