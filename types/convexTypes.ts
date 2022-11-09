26ea73742a75c3f8eac3c35fb15068e8a223cb67
tushargargght <tushargargght@gmail.com>
2022-11-09 17:09:59 +0530

    feat: Added convex FraxBp types

--- types/convexTypes.ts
+++ types/convexTypes.ts
@@ -1,9 +1,12 @@
 import type { BigNumberish } from "ethers"
 
-import type { Convex3CrvBasicVault, Convex3CrvLiquidatorVault } from "./generated"
+import type { Convex3CrvBasicVault, Convex3CrvLiquidatorVault, ConvexFraxBpBasicVault, ConvexFraxBpLiquidatorVault } from "./generated"
 
 export type Convex3CrvPoolTypes = "musd" | "pwrd" | "usdp" | "mim"
 export type Convex3CrvPoolFactory = "basic" | "liquidator"
+
+export type ConvexFraxBpPoolTypes = "pusd" | "lusd" | "alusd" | "busd" | "tusd" | "susd"
+export type ConvexFraxBpPoolFactory = "basic" | "liquidator"
 
 /**
  * Meta vault parameters for  Convex Pools
@@ -12,6 +15,39 @@ export type Convex3CrvPoolFactory = "basic" | "liquidator"
  * @interface Convex3CrvPool
  */
 export interface Convex3CrvPool {
+    /** Curve Metapool */
+    curveMetapool: string // dif
+    metapoolDepositZap?: string
+    isFactory: boolean
+    /** Curve Metapool Token*/
+    curveMetapoolToken: string // dif
+    convexPoolId: number // dif
+    convexRewardPool: string
+    name: string
+    symbol: string
+    decimals: number
+    asset: string
+    // liquidator settings
+    streamDuration?: BigNumberish
+    rewardTokens?: string[]
+    donateToken?: string
+    feeReceiver?: string
+    donationFee?: number
+    slippageData: {
+        redeem: number
+        deposit: number
+        withdraw: number
+        mint: number
+    }
+}
+
+/**
+ * Meta vault parameters for  Convex Pools
+ *
+ * @export
+ * @interface ConvexFraxBpPool
+ */
+ export interface ConvexFraxBpPool {
     /** Curve Metapool */
     curveMetapool: string // dif
     metapoolDepositZap?: string
@@ -54,6 +90,20 @@ export interface Convex3CrvConstructorData {
     convexPoolId: BigNumberish
 }
 
+/**
+ * Convex FraxBp constructor struct
+ *
+ * @export
+ * @interface ConvexFraxBpConstructorData
+ */
+ export interface ConvexFraxBpConstructorData {
+    /** Curve Metapool Convex3CrvPool.curveMetapool */
+    metapool: string
+    booster: string
+    /** Convex3CrvPool.convexPoolId */
+    convexPoolId: BigNumberish
+}
+
 // Convex 3RCV Vault Configurations
 export type Convex3CrvVaultsConstructors = {
     [key in Convex3CrvPoolTypes]?: Convex3CrvConstructorData
@@ -62,7 +112,16 @@ export type Convex3CrvVaultsConfig = {
     [key in Convex3CrvPoolTypes]?: Convex3CrvPool
 }
 
+// Convex FraxBp Vault Configurations
+export type ConvexFraxBpVaultsConstructors = {
+    [key in ConvexFraxBpPoolTypes]?: ConvexFraxBpConstructorData
+}
+export type ConvexFraxBpVaultsConfig = {
+    [key in ConvexFraxBpPoolTypes]?: ConvexFraxBpPool
+}
+
 export type Convex3CrvVault = Convex3CrvLiquidatorVault | Convex3CrvBasicVault
+export type ConvexFraxBpVault = ConvexFraxBpLiquidatorVault | ConvexFraxBpBasicVault
 
 /**
  * Meta vault parameters for  Curve Pools
@@ -71,6 +130,27 @@ export type Convex3CrvVault = Convex3CrvLiquidatorVault | Convex3CrvBasicVault
  * @interface Convex3CrvPool
  */
 export interface Curve3CrvPool {
+    metaVault?: string
+    name: string
+    symbol: string
+    decimals: number
+    asset: string
+    // TODO - review if it can be removed from here
+    slippageData: {
+        redeem: number
+        deposit: number
+        withdraw: number
+        mint: number
+    }
+}
+
+/**
+ * Meta vault parameters for  Curve Pools
+ *
+ * @export
+ * @interface ConvexFraxBpPool
+ */
+ export interface CurveFraxBpPool {
     metaVault?: string
     name: string
     symbol: string
